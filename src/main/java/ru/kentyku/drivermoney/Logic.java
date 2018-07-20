b9b2a1311074f5a492d4a646cbd1d7c4eea8294c
kentyku <kentyku@box>
2018-07-20 23:38:39 +0400

    реализую класс selectcommand() реализовал метод добавления водителя

--- src/main/java/ru/kentyku/drivermoney/Logic.java
+++ src/main/java/ru/kentyku/drivermoney/Logic.java
@@ -4,29 +4,69 @@
  */
 package ru.kentyku.drivermoney;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 /**
- *Класс описывающий логику работы приложения
+ * Класс описывающий логику работы приложения
+ *
  * @author kentyku
  */
 public class Logic {
-    void ReadingToBaze(){
+
+    String typedriver;
+
+    //консольный выбор команд
+    void selectcommand() throws ClassNotFoundException, SQLException {
+        String command = "";
+
+        while (true) {
+            System.out.println("Выберите требуемое действие");
+            //сканер для ввода команды
+            Scanner sc = new Scanner(System.in);
+            command = sc.nextLine();
+
+            //добавление нового водителя
+            if (command.equals("/add")) {
+                System.out.println("Выберите стратегию оплаты добавляемого водителя");
+                command = sc.nextLine();
+                if (command.equals("everymonth")) {
+                    DriverMonth dm = new DriverMonth();
+                    ReadingToBaze rtb = new ReadingToBaze();
+                    rtb.connect();
+                    System.out.println("m");
+                }
+                if (command.equals("everyhour")) {
+                    DriverHour dh = new DriverHour();
+                    System.out.println("h");
+                }
+            }
+
+            //Выход из программы
+            if (command.equals("/exit")) {
+                System.exit(0);
+            }
+//            break;//заглушка против зацикливания
+        }
+        //сканер для ввода команды
+
+    }
+
+    void ReadingToBaze() {
         //Создание класса обработчика соединений с БД. Содержит все методы для работы с базой
     }
-    
-    
-    void checkFile(){
+
+    void checkFile() {
         //проверка корректности загружаемого файла
     }
-    
-    void loadingList(){        
+
+    void loadingList() {
         //Загружаем список водителей из файла в БД программы.
         checkFile();
     }
-    
-    void SaveList(){
+
+    void SaveList() {
         //Сохраняем список водителей из БД программы в файл.
     }
-    
-    
-    
+
 }
